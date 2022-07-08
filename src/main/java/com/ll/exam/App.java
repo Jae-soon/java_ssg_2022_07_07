7a5b8e1ca53b05d8cfd384a98724d62bff3cbedd
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-07-08 13:39:57 +0900

    dependency 추가

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -21,12 +21,18 @@ public class App {
                     String sentence = sc.nextLine().trim();
                     System.out.print("작가 : ");
                     String author = sc.nextLine().trim();
-                    int id = wiseSayingIndexId++;
+                    int id = ++wiseSayingIndexId;
 
                     WiseSaying wiseSaying = new WiseSaying(id, sentence, author);
 
                     System.out.println(id + "번 명령이 등록되었습니다.");
                     break;
+
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("-----------------");
+                    System.out.println(wiseSaying.toString());
+
 
                 case "종료":
                     break outer;
