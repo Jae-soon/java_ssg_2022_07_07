148d110a98047f60d0bc3b2f690390c87321fa5c
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-07-12 15:33:11 +0900

    하위 귀관 위임

--- src/main/java/com/ll/exam/WiseSayingController.java
+++ src/main/java/com/ll/exam/WiseSayingController.java
@@ -1,6 +1,5 @@
 package com.ll.exam;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,8 +15,11 @@ public class WiseSayingController {
     public void list(Rq rq) {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("-------------------");
-        for (int i = wiseSayingRepository.wiseSayings.size() - 1; i >= 0; i--) {
-            WiseSaying wiseSaying_ = wiseSayingRepository.wiseSayings.get(i);
+
+        List<WiseSaying> wiseSayings = wiseSayingRepository.findAll();
+
+        for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+            WiseSaying wiseSaying_ = wiseSayings.get(i);
             System.out.printf("%d / %s / %s\n", wiseSaying_.id, wiseSaying_.sentence, wiseSaying_.author);
         }
     }
@@ -27,12 +29,10 @@ public class WiseSayingController {
         String content = sc.nextLine().trim();
         System.out.printf("작가 : ");
         String author = sc.nextLine().trim();
-        int id = ++wiseSayingRepository.wiseSayingLastId; // 명언 글 번호 증가
 
-        WiseSaying wiseSaying = new WiseSaying(id, content, author);
-        wiseSayingRepository.wiseSayings.add(wiseSaying);
+        WiseSaying wiseSaying = wiseSayingRepository.write(content, author);
 
-        System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+        System.out.printf("%d번 명언이 등록되었습니다.\n", wiseSaying.id);
     }
 
     public void modify(Rq rq) {
@@ -43,47 +43,41 @@ public class WiseSayingController {
             return;
         }
         WiseSaying foundwiseSaying = wiseSayingRepository.findById(paramId);
-        int id = foundwiseSaying.id;
-        String sentence = foundwiseSaying.sentence;
 
         if (foundwiseSaying == null) {
             System.out.printf("%d번 명언은 존재하지 않습니다..\n", paramId);
             return;
         }
 
-        System.out.printf("%d번 명언을 수정합니다.\n", id);
+        System.out.printf("%d번 명언을 수정합니다.\n", paramId);
         System.out.printf("기존 명언 : %s\n", foundwiseSaying.sentence);
         System.out.printf("기존 작가 : %s\n", foundwiseSaying.author);
         System.out.print("새 명언 : ");
-        foundwiseSaying.sentence = sc.nextLine().trim();
+        String sentence = sc.nextLine().trim();
         System.out.print("새 작가 : ");
-        foundwiseSaying.author = sc.nextLine().trim();
+        String author = sc.nextLine().trim();
 
-        System.out.printf("%d번 명언이 수정되었습니다.\n", id);
+        wiseSayingRepository.modify(paramId, sentence, author);
+
+        System.out.printf("%d번 명언이 수정되었습니다.\n", paramId;
     }
 
     public void remove(Rq rq) {
-        // URL에 입력된 id 얻기
         int paramId = rq.getIntParam("id", 0);
 
-        // URL에 입력된 id가 없다면 작업중지
         if (paramId == 0) {
             System.out.println("id를 입력해주세요.");
             return;
         }
 
-        // URL에 입력된 id에 해당하는 명언객체 찾기
         WiseSaying foundWiseSaying = wiseSayingRepository.findById(paramId);
 
-        // 찾지 못했다면 중지
         if (foundWiseSaying == null) {
             System.out.printf("%d번 명언은 존재하지 않습니다.\n", paramId);
             return;
         }
 
-        // 입력된 id에 해당하는 명언객체를 리스트에서 삭제
-        wiseSayingRepository.wiseSayings.remove(foundWiseSaying);
-
+        wiseSayingRepository.remove(paramId);
         System.out.printf("%d번 명언이 삭제되었습니다.\n", paramId);
     }
 }
